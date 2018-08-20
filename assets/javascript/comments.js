a3400b91d179b115d6abee619c63bda7d5d34634
Amy Moretti <amyemoretti@gmail.com>
2018-08-20 13:18:43 -0500

    Adding all that firebase

--- assets/javascript/comments.js
+++ assets/javascript/comments.js
@@ -1,57 +1,40 @@
 var name;
-var destination;
-var start;
-var frequency;
+var shelter;
+var story;
 
 var database = firebase.database();
 
 // On click submit, pushes info to Firebase
-$("#submit-button").on("click", function (event) {
+$("#submit").on("click", function (event) {
     event.preventDefault();
 
-    name = $("#train-name").val().trim();
-    destination = $("#destination").val().trim();
-    start = $("#start-time").val();
-    frequency = $("#frequency").val().trim();
+    name = $("#name").val().trim();
+    shelter = $("#shelter").val().trim();
+    story = $("#story").val();
 
     database.ref().push({
         name: name,
-        destination: destination,
-        start: start,
-        frequency: frequency
+        shelter: shelter,
+        story: story,
     });
 
 
-    $("#train-form").trigger("reset")
+    $(".input-field").trigger("reset")
 })
 
+database.ref().on("child_added", function (snapshot) {
+    var dbName = snapshot.val().name;
+    var dbShelter = snapshot.val().shelter;
+    var dbStory = snapshot.val().story;
 
-
-// Function for updating train table / Time uses Moment.js
-function table(train) {
-    startTime = moment(train.start, "HH:mm").add(-1, "days");
-    var currentTime = moment();
-    var timeDifference = currentTime.diff(startTime, "minutes");
-    var lastTrain = timeDifference % train.frequency;
-    console.log("before add", currentTime)
-    var nextTrain = currentTime.add(train.frequency - lastTrain, "minutes")
-    console.log("after add", currentTime)
-    var nextTrainTime = nextTrain.format("h:mm: A")
-    var minutesAway = nextTrain.diff(moment(), "minutes")
 
 
     // Update table
     var newRow = $("<tr>");
-    newRow.append($("<td>").text(train.name));
-    newRow.append($("<td>").text(train.destination));
-    newRow.append($("<td>").text(train.frequency));
-    newRow.append($("<td>").text(nextTrainTime));
-    newRow.append($("<td>").text(minutesAway));
-    $("#table-body").append(newRow);
-}
-
-database.ref().on("child_added", function (snapshot) {
-    var snap = snapshot.val();
-    table(snap);
+    newRow.append($("<td>").text(dbName));
+    newRow.append($("<td>").text(dbShelter));
+    newRow.append($("<td>").text(dbStory));
+    $("#tbody").append(newRow);
 
 });
+
